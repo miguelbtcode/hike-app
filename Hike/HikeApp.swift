e00151ac1630de75e303c695bc079b351617b701
mikecode <102126994+miguelbtcode@users.noreply.github.com>
2024-12-21 18:47:25 -0500

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Miguel Barreto on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
